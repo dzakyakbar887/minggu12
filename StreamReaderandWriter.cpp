bfa74807e993cee12fbfc680ac60d0690437c4b8
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-06-02 17:39:32 +0700

    string baris

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -3,3 +3,13 @@
 #include <string>
 using namespace std;
 
+int main()
+{
+
+    string baris;
+
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    
